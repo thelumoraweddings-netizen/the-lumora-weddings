a3a8533c81e6a8bec16f464a58d75955c1fb6c7d
Prabavathi <135326981+prabavathimagesh@users.noreply.github.com>
2026-04-22 00:06:39 +0530

    Ensure footer Home link scrolls to top

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                     <div className="col-span-2 footer-nav-col">
                         <h4 className="footer-heading-modern">Studio</h4>
                         <ul className="footer-list-modern">
-                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/" onClick={() => window.scrollTo(0, 0)}>Home</Link></li>
                             <li><Link to="/gallery">Wedding Films</Link></li>
                             <li><Link to="/about">Our Story</Link></li>
                         </ul>
